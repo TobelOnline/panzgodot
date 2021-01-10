b49d281f03a27630309158c9ac15b586208a68d7
Tobel <tdr@tobel-online.de>
2021-01-10 17:52:44 +0100

    network update

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 # Declare member variables here. Examples:
 var debug = true
 
@@ -13,5 +12,7 @@ var gameMode : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("GEht los")
+	
+#	pass # Replace with function body.
 
