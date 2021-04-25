5ddf4634f78134ca4666b428181006fea8505edf
Tobias Dreyschultze <tdr@tobel-online.de>
2021-04-25 22:44:41 +0200

    network sync

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -31,4 +31,4 @@ func _process(delta):
 	timer += delta
 	if timer > TIMER_LIMIT: # Prints every 2 seconds
 		timer = 0.0
-		print("fps: " + str(Engine.get_frames_per_second()))
+		#print("fps: " + str(Engine.get_frames_per_second()))
