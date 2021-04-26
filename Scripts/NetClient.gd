e00f9619e5f1a2c2268d6231b17cba7bfb682697
Tobias Dreyschultze <tdr@tobel-online.de>
2021-04-26 19:36:17 +0200

    add network client

---
+++ Scripts/NetClient.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var network = NetworkedMultiplayerENet.new()
+var ip = "127.0.0.1"
+var port = 1990
+
+func _ready():
+	ConnectToServer()
+	
+func ConnectToServer():
+	network.create_client(ip, port)
+	get_tree().set_network_peer(network)
+	network.connect("connection_failed", self, "_OnConnectionFailed")
+	network.connect("connection_succeeded", self, "_OnConnectionSucceeded")
+	
+func _OnConnectionFailed():
+	print("Failed to connect to server")
+	
+func _OnConnectionSucceeded():
+	print("Succesfully connected")
